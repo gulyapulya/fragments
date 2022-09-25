194dddf03dbaf3d7f296406c5dac290148c98e0b
Gulnur Baimukhambetova <gulnur2703@gmail.com>
2022-09-24 22:39:40 -0400

    Completed HTTP Responses (section 3) unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
     node: true,
     commonjs: true,
     es2021: true,
+    jest: true,
   },
   extends: ['eslint:recommended'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 13,
   },
   rules: {},
 };
