256899e7a8845d722a73203c17292a961b56fa15
Gulnur Baimukhambetova <gulnur2703@gmail.com>
2022-10-09 14:58:35 -0400

    Added GET route with unit tests

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -2,17 +2,17 @@
 
 //Successful and error response functions
 const response = require('../../response');
+const { Fragment } = require('../../model/fragment');
 
 /**
  * Get a list of fragments for the current user
  */
- module.exports = (req, res) => {
-    // TODO: this is just a placeholder to get something working...
-    res.status(200).json(response.createSuccessResponse({fragments: []}));
-    /** 
-    * Old version of sending response
-    res.status(200).json({
-      status: 'ok',
-      fragments: [],
-    });*/
-  };
+module.exports = async (req, res) => {
+  try {
+    let expand = req.query.expand && req.query.expand === '1' ? true : false;
+    const fragments = await Fragment.byUser(req.user, expand);
+    res.status(200).json(response.createSuccessResponse({ fragments }));
+  } catch (err) {
+    res.status(401).json(response.createErrorResponse(401, err));
+  }
+};
