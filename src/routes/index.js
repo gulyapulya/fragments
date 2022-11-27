4d19fe641749767b5da1ef2dba45ed375436e1b8
Gulnur Baimukhambetova <gulnur2703@gmail.com>
2022-11-27 18:33:28 -0500

    Added host name to the health check

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,11 +14,13 @@ const response = require('../response');
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+const { hostname } = require('os');
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  * Protect them all so you have to be authenticated in order to access.
  */
- router.use(`/v1`, authenticate(), require('./api'));
+router.use(`/v1`, authenticate(), require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
@@ -28,11 +30,13 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json(response.createSuccessResponse(
-    {
-      author, 
-      githubUrl: 'https://github.com/gulyapulya/fragments', 
-      version
+  res.status(200).json(
+    response.createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/gulyapulya/fragments',
+      version,
+      // Include the hostname in the response
+      hostname: hostname(),
     })
   );
 });
